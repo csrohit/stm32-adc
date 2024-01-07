aa77f1b1b5fffd4ed38fb306b32ba464de7069ee
Rohit Nimkar <nehalnimkar@gmail.com>
2024-01-07 11:39:07 +0530

    📝 update comments

--- interrupt/src/startup_stm32f1.c
+++ interrupt/src/startup_stm32f1.c
@@ -1,15 +1,15 @@
 /**
- * @file startup_stm32f1.c
- * @author Rohit Nimkar (nehalnimkar@gmail.com)
- * @brief startup declarations and function for the STM32F1 series controllers
- *        See: RM0008 10.1.2 Interrupt and exception vectors, Table 63. Vector table for other STM32F10xxx devices
- * 
- * @version 1.2
- * @date 2022-12-07
- * 
- * @copyright Copyright (c) 2022
+ * @file     startup_stm32f1.c
+ * @author   Rohit Nimkar <https://www.linkedin.com/in/rohit-nimkar>
+ * @brief    startup declarations and function for the STM32F1 series controllers
+ *           See: RM0008 10.1.2 Interrupt and exception vectors, Table 63. Vector table for other STM32F10xxx devices
+ *
+ * @version  1.3
+ * @date     2024-01-06
+ *
+ * @copyright Copyright (c) 2024
+ *
  * @attention
- * 
  * This software component is licensed by Rohit Nimkar under BSD 3-Clause license,
  * the "License"; You may not use this file except in compliance with the
  * License. You may obtain a copy of the License at: opensource.org/licenses/BSD-3-Clause
@@ -17,9 +17,9 @@
  */
 
 #include <stdint.h>
-#include <stm32f1xx.h>
-#include <main.h>
-#include <startup_stm32f1.h>
+#include "startup_stm32f1.h"
+#include "interrupt.h"
+#include "main.h"
 
 // Define the vector table
 uint32_t vectors[] __attribute__((section(".isr_vector"))) = {
@@ -30,20 +30,20 @@ uint32_t vectors[] __attribute__((section(".isr_vector"))) = {
     (uint32_t)MemManage_Handler,
     (uint32_t)BusFault_Handler,
     (uint32_t)UsageFault_Handler,
-    0,        // reserved
-    0,        // reserved
-    0,        // reserved
-    0,        // reserved
+    0, // reserved
+    0, // reserved
+    0, // reserved
+    0, // reserved
     (uint32_t)SVC_Handler,
     (uint32_t)DebugMon_Handler,
-    0,        // reserved
+    0, // reserved
     (uint32_t)PendSV_Handler,
     (uint32_t)SysTick_Handler,
     (uint32_t)WWDG_IRQHandler,
     (uint32_t)PVD_IRQHandler,
     (uint32_t)TAMP_STAMP_IRQHandler,
     (uint32_t)RTC_WKUP_IRQHandler,
-    0,        // Flash global interrupt
+    0, // Flash global interrupt
     (uint32_t)RCC_IRQHandler,
     (uint32_t)EXTI0_IRQHandler,
     (uint32_t)EXTI1_IRQHandler,
@@ -84,38 +84,35 @@ uint32_t vectors[] __attribute__((section(".isr_vector"))) = {
     (uint32_t)USB_Wakeup_IRQHandler,
 };
 
-
 void Default_Handler(void)
 {
-  while (1)
-    ;
+    while (1)
+        ;
 }
-
 
 void __attribute__((noreturn)) Reset_Handler(void)
 {
-  // copy .data section to SRAM
-  uint32_t *start_sram = (uint32_t *)&_sdata;
-  uint32_t *start_flash = (uint32_t *)&_la_data;
-  while (start_sram < (uint32_t *)&_edata)
-  {
-    *start_sram++ = *start_flash++;
-  }
+    // copy .data section to SRAM
+    uint32_t *start_sram  = (uint32_t *)&_sdata;
+    uint32_t *start_flash = (uint32_t *)&_la_data;
+    while (start_sram < (uint32_t *)&_edata)
+    {
+        *start_sram++ = *start_flash++;
+    }
 
-  // copy .bss section to SRAM
-  uint32_t *start_bss = (uint32_t *)&_sbss;
-  while (start_bss < (uint32_t *)&_ebss)
-  {
-    *start_bss++ = 0;
-  }
-  
-  // Call main function
-  main();
+    // copy .bss section to SRAM
+    uint32_t *start_bss = (uint32_t *)&_sbss;
+    while (start_bss < (uint32_t *)&_ebss)
+    {
+        *start_bss++ = 0;
+    }
 
-  // loop continuously
-  while (1)
-  {
-    __asm("nop");
-  }
-  
+    // Call main function
+    main();
+
+    // loop continuously
+    while (1)
+    {
+        __asm("nop");
+    }
 }
